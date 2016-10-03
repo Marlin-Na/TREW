0f051c8ca0785aeb8e507de238043db8fbb64b9b
Marlin- <marlin-@gmx.cn>
2016-10-04 07:58:17 +0800

    Reformat code

--- server.R
+++ server.R
@@ -1,17 +1,27 @@
 function(input, output) {
-  tb1 <- eventReactive(input$button,{Tb1(Target = input$pro,
-                                         Modification = input$mod,
-                                         Gene_ID = input$gene
-  )
-  }
-  )
-  tb2 <- reactive({Tb2(tb1())})
-  
-  output$table <- DT::renderDataTable(Tb2_DT(tb2()), server = TRUE)
-  
-  tb3 <- eventReactive(input$table_rows_selected,{Tb3(Tb1 = tb1(),
-                                                      Tb2 = tb2(),
-                                                      Select_Number = as.numeric(input$table_rows_selected))})
-  output$table2 <- DT::renderDataTable(Tb3_DT(tb3()), server = TRUE)
-  
+    
+    tb1 <- eventReactive(input$button, {
+        Tb1(
+            Target = input$pro,
+            Modification = input$mod,
+            Gene_ID = input$gene
+        )
+    })
+    
+    tb2 <- reactive({
+        Tb2(tb1())
+    })
+    
+    output$table <- DT::renderDataTable(Tb2_DT(tb2()), server = TRUE)
+    
+    tb3 <- eventReactive(input$table_rows_selected, {
+        Tb3(
+            Tb1 = tb1(),
+            Tb2 = tb2(),
+            Select_Number = as.numeric(input$table_rows_selected)
+        )
+    })
+    
+    output$table2 <- DT::renderDataTable(Tb3_DT(tb3()), server = TRUE)
+    
 }
