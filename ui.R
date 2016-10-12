0a89c67785ed431b6ddadaadfcfe834f53db368b
Marlin- <marlin-@gmx.cn>
2016-10-12 13:20:20 +0800

    Reformat ui.R

--- ui.R
+++ ui.R
@@ -1,39 +1,43 @@
 fluidPage(
-
-  titlePanel("Basic DataTable"),
-  fluidRow(
-    column(4,
-           selectInput("mod",
-                       "Modifications:",
-                       c("All","m6A","m5C"))
+    titlePanel("Basic DataTable"),
+    fluidRow(
+        column(width = 4,
+            selectInput(
+                "mod",
+                "Modifications:",
+                c("All", "m6A", "m5C")
+            )
+        ),
+        column(width = 4,
+            selectInput(
+                "pro",
+                "Proteins:",
+                c("All", "Regulator", "Regulatee")
+            )
+        ),
+        column(width = 4,
+            textInput("gene", "Genes:", width = 600, placeholder = "e.g cdk"),
+            actionButton("submitQuery", "Submit")
+        )
     ),
-    column(4,
-           selectInput("pro",
-                       "Proteins:",
-                       c("All","Regulator","Regulatee"))
+    fluidRow(DT::dataTableOutput("table")),
+    fluidRow(DT::dataTableOutput("table2")),
+    
+    ## Jbrowse navigation
+    fluidRow(
+        h2('Genome Browser'),
+        textOutput(outputId = 'outGene', inline = T),
+        selectInput(
+            inputId = 'inGenome',
+            label = 'Genome Assembly',
+            choices = 'Not Available'
+        )
     ),
-    column(4,textInput("gene", "Genes:", width = 600, placeholder = "e.g cdk"),
-           actionButton("submitQuery", "Submit")
+    
+    ## Jborwse iframe UI
+    fluidRow(
+        column(width = 12,
+            uiOutput(outputId = 'outJbrowse')
+        )
     )
-  ),
-  fluidRow(
-    DT::dataTableOutput("table")
-  ),
-  fluidRow(
-    DT::dataTableOutput("table2")
-  ),
-  
-  ## Jbrowse navigation
-  fluidRow(
-    h2('Genome Browser'),
-    textOutput(outputId='outGene',inline = T),
-    selectInput(inputId='inGenome',label='Genome Assembly',choices='Not Available')
-  ),
-  
-  ## Jborwse iframe UI
-  fluidRow(
-    column(width = 12,
-      uiOutput(outputId = 'outJbrowse')
-    )
-  )
 )
